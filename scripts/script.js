1215a1b01064aa73eb1c0dba8f811f76a7d2fa04
Akvis24 <akvis2022@gmail.com>
2026-02-05 13:25:26 +0100

    script.js animation added

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,15 @@
+const inputRef = document.getElementById("search");
+const mainRef = document.getElementById("main-container");
 
+
+const pokemons = [];
 
 
 function init() {
-    
+    loadAnimation();
+}
+
+function loadAnimation() {
+    mainRef.innerHTML = "";
+    mainRef.innerHTML = `<img class="loading" src="./assets/icons/pokemon-icon.png">`;
 }
