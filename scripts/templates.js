b38e7729e8a6288d986129984fd1c90471d43eb8
Akvis24 <akvis2022@gmail.com>
2026-02-11 01:02:34 +0100

    update

--- scripts/templates.js
+++ scripts/templates.js
@@ -16,23 +16,18 @@ function loadPokemonInfo(i) {
     return `
         <div class="infoCard">
             <div class="image-shortInfo ${pokemonDatas[i].types[0].type.name}">
-                <img class="pokemon-main-image"
-                     src="${pokemonDatas[i].sprites.other.home.front_default}">
-                
+                <img class="pokemon-main-image" src="${pokemonDatas[i].sprites.other.home.front_default}">
                 <div class="short-info">
                     <h2>${pokemonDatas[i].name}</h2>
                     <p>Base Experience: ${pokemonDatas[i].base_experience}</p>
                     <p>Height: ${pokemonDatas[i].height}</p>
                     <p>Weight: ${pokemonDatas[i].weight}</p>
                     <p>Type: ${getPokemonTypes(i)}</p><br>
-
                     <div class="nav-buttons">
                         <button class="button" onclick="showPreviousPokemon()">⬅️ Previous</button>
                         <button class="button" onclick="showNextPokemon()">Next ➡️</button>
                     </div>
-
-                    <button style="width: 100%" class="back-btn button"
-                            onclick="renderPokemons()">Back</button>
+                    <button style="width: 100%" class="back-btn button" onclick="renderPokemons()">Back</button>
                 </div>
             </div>
         </div>
